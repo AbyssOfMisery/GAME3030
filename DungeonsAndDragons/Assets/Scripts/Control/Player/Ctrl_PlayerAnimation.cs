5bc4d2eac15d54b3df3d39da806469cb9b566579
Xian767-6424 <837323359@qq.com>
2020-01-23 17:15:07 -0500

    add skeleton script file
    add skeleton script file

---
+++ DungeonsAndDragons/Assets/Scripts/Control/Player/Ctrl_PlayerAnimation.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Title:"Dungoen and dragons"
+ *      
+ *      Control layer: Animation control
+ *      
+ * Description:
+ *         
+ * Date:2020
+ * 
+ * Verstion: 0.1
+ * 
+ * Modify Recoder;
+ *  
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Control {
+    public class Ctrl_PlayerAnimation : BaseControl
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
---
+++ DungeonsAndDragons/Assets/Scripts/Control/Player/Ctrl_PlayerMovingCtrlByKey.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Title:"Dungoen and dragons"
+ *      
+ *      Control layer: use keyboard to move player
+ *      
+ * Description:
+ *         
+ * Date:2020
+ * 
+ * Verstion: 0.1
+ * 
+ * Modify Recoder;
+ *  
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Control
+{
+    public class Ctrl_PlayerMovingCtrlByKey : BaseControl
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
