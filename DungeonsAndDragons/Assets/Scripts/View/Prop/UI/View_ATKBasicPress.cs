28ee7e07139653125b5e0ee7d78d395bcea320af
Xian767-6424 <837323359@qq.com>
2020-03-09 20:32:44 -0400

    bake light map
    bake light map

---
+++ DungeonsAndDragons/Assets/Scripts/View/Prop/UI/View_ATKBasicPress.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Title:"Dungoen and dragons"
+ *      
+ *     View Mode: UI buttom animation
+ *      
+ * Description:
+ *    do response to click event
+ *      
+ * Date:2020
+ * 
+ * Verstion: 0.1
+ * 
+ * Modify Recoder;
+ *  
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Control;
+using Kernal;
+using Global;
+
+namespace View
+{
+    public class View_ATKBasicPress : StateMachineBehaviour
+    {
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    
+        //}
+
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            // Debug.Log("OnStateUpdate");
+            if(UnityHelper.GetInstance().GetSmallTime(GlobalParameter.INTERVAL_TIME_0DOT2))
+            {
+                Ctrl_PlayerAttackInputByET.Instance.ResponseATKByBasic();
+            }
+
+        }
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    
+        //}
+
+        // OnStateMove is called right after Animator.OnAnimatorMove()
+        //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that processes and affects root motion
+        //}
+
+        // OnStateIK is called right after Animator.OnAnimatorIK()
+        //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    // Implement code that sets up animation IK (inverse kinematics)
+        //}
+    }
+}
+
